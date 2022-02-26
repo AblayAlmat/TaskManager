8096f565562b40417a2d1985ff0d050c4c947733
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-26 17:05:42 +0600

    Добавил применение конфигурации при создании модели

--- TaskManager/Models/DbContext/TaskManagerContext.cs
+++ TaskManager/Models/DbContext/TaskManagerContext.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using TaskManager.Models.DbMapConfigurations;
 
 namespace TaskManager.Models.DbContext
 {
@@ -10,6 +11,12 @@ namespace TaskManager.Models.DbContext
         public TaskManagerContext(DbContextOptions<TaskManagerContext> options) : base(options)
         {
             
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            builder.ApplyConfiguration(new TaskDbMap());
         }
     }
 }
