2fbf9e4109d72eb58e1ee706ac0398caa5d0d46a
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 12:56:16 +0600

    Добавил модель представления для регистрациии пользователя

---
+++ TaskManager/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.ViewModels
+{
+    public class RegisterViewModel
+    {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Введите адрес эл.почты")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Подтвердите пароль")]
+        [Compare("Password", ErrorMessage = "Пароли не совпадают")]
+        [DataType(DataType.Password)]
+        public string PasswordConfirmation { get; set; }
+        public string Role { get; set; }
+    }
+}
