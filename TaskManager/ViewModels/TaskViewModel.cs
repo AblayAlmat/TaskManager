5df7ec40a1c674c8aa72eb15e2b75b699cc0c2f9
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 12:54:50 +0600

    Добавил модель представления задачи

---
+++ TaskManager/ViewModels/TaskViewModel.cs
@@ -0,0 +1,18 @@
+using System;
+using TaskManager.Enums;
+
+namespace TaskManager.ViewModels
+{
+    public class TaskViewModel
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Priority Priority { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime FinishingDate { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatorName { get; set; }
+        public string ExecutorName { get; set; }
+        public Status Status { get; set; }
+    }
+}
