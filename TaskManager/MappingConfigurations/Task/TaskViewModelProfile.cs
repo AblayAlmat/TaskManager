32bd8652d32b3f34e0dc31ae0662ffd4e29c1974
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 12:55:41 +0600

    Добавил конфигурацию для конвертации из модели Task в модель представления TaskViewModel

---
+++ TaskManager/MappingConfigurations/Task/TaskViewModelProfile.cs
@@ -0,0 +1,31 @@
+using AutoMapper;
+using TaskManager.ViewModels;
+
+namespace TaskManager.MappingConfigurations.Task
+{
+    public class TaskViewModelProfile : Profile
+    {
+        public TaskViewModelProfile()
+        {
+            CreateMap<Models.Task, TaskViewModel>()
+                .ForMember(dest => dest.Name, opt =>
+                    opt.MapFrom(t => t.Name))
+                .ForMember(dest => dest.Description, opt =>
+                    opt.MapFrom(t => t.Description))
+                .ForMember(dest => dest.Priority, opt =>
+                    opt.MapFrom(t => t.Priority))
+                .ForMember(dest => dest.Deadline, opt =>
+                    opt.MapFrom(t => t.Deadline))
+                .ForMember(dest => dest.CreationDate, opt =>
+                    opt.MapFrom(t => t.CreationDate))
+                .ForMember(dest => dest.FinishingDate, opt =>
+                    opt.MapFrom(t => t.FinishingDate))
+                .ForMember(dest => dest.CreatorName, opt =>
+                    opt.MapFrom(t => t.Creator.UserName))
+                .ForMember(dest => dest.ExecutorName, opt =>
+                    opt.MapFrom(t => t.Executor.UserName))
+                .ForMember(dest => dest.Status, opt =>
+                    opt.MapFrom(t => t.Status));
+        }
+    }
+}
