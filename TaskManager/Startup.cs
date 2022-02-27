91eeecbdc997c16a1059d72f925740639b7c6cd1
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 15:28:28 +0600

    Подключил методы для регистрации репозиториев и сервисов

--- TaskManager/Startup.cs
+++ TaskManager/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using TaskManager.Extensions;
 using TaskManager.Models;
 using TaskManager.Models.DbContext;
 using TaskManager.Services.RoleSeederService;
@@ -34,6 +35,8 @@ namespace TaskManager
             services.AddAutoMapper(typeof(Startup));
             services.AddTransient<RoleSeederService>();
             services.AddTransient<UserSeederService>();
+            services.AddServices();
+            services.AddRepositories();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
