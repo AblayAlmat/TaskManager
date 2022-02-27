2454fbf4f96d890d0ba9400b65ac37e82ead57e9
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 13:00:21 +0600

    Добавил файлы миграций базы данных при добавлении свойства статуса выполнения задачи и обновил снэпшот контекста базы данных

--- TaskManager/Migrations/20220227060625_StatusPropertyAddedToTaskEntity.Designer.cs
+++ TaskManager/Migrations/20220227060625_StatusPropertyAddedToTaskEntity.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TaskManager.Models.DbContext;
@@ -9,9 +10,10 @@ using TaskManager.Models.DbContext;
 namespace TaskManager.Migrations
 {
     [DbContext(typeof(TaskManagerContext))]
-    partial class TaskManagerContextModelSnapshot : ModelSnapshot
+    [Migration("20220227060625_StatusPropertyAddedToTaskEntity")]
+    partial class StatusPropertyAddedToTaskEntity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
