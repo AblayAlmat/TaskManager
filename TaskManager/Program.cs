38ba9ac7e9452ca647d46963c835dfb674735501
umirkhanov.k <umirkhanov.k@halykSK.kz>
2022-02-27 12:44:47 +0600

    Добавил вызов методов по добавлению ролей и пользователей

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -1,19 +1,37 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using TaskManager.Models.DbContext;
+using TaskManager.Services.RoleSeederService;
+using TaskManager.Services.UserSeederService;
 
 namespace TaskManager
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using var scope = host.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            try
+            {
+                var context = services.GetRequiredService<TaskManagerContext>();
+                await context.Database.MigrateAsync();
+                RoleSeederService roleSeeder = new RoleSeederService(context);
+                await roleSeeder.SeedRoles();
+                UserSeederService userSeeder = new UserSeederService(context);
+                await userSeeder.SeedUsers();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
