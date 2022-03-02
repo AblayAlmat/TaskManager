3b5f9cea2b1e191f13defc392b87cc33c2f68dc1
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-02 17:49:02 +0600

    Добавил модель представления для создания задачи

---
+++ TaskManager/ViewModels/TaskCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TaskManager.Enums;
+
+namespace TaskManager.ViewModels
+{
+    public class TaskCreateViewModel
+    {
+        [Required(ErrorMessage = "Введите название задачи")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Введите описание задачи")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Выберите приоритет задачи")]
+        public Priority Priority { get; set; }
+        [Required(ErrorMessage = "Введите дату дедлайна задачи")]
+        public DateTime Deadline { get; set; }
+        public DateTime CreationDate { get; set; }
+        public string CreatorId { get; set; }
+        public Status Status { get; set; }
+    }
+}
