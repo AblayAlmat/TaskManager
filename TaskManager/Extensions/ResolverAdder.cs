65a7656c311860adc8703d971f5c3d4a9eba11cc
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-02 17:59:09 +0600

    Добавил регистрацию resolver-а по поиску пользователя по id

---
+++ TaskManager/Extensions/ResolverAdder.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using TaskManager.MappingConfigurations.Resolvers;
+
+namespace TaskManager.Extensions
+{
+    public static class ResolverAdder
+    {
+        public static void AddResolvers(this IServiceCollection services)
+        {
+            services.AddScoped<GetUserResolver>();
+        }
+    }
+}
