7bbe613ef8af61ff8b6486bc892cf7b5d6185fc7
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-02 17:58:38 +0600

    Добавил resolver для получения пользователя по id

---
+++ TaskManager/MappingConfigurations/Resolvers/GetUserResolver.cs
@@ -0,0 +1,22 @@
+﻿using AutoMapper;
+using TaskManager.Models;
+using TaskManager.Repositories.UserRepository;
+using TaskManager.ViewModels;
+
+namespace TaskManager.MappingConfigurations.Resolvers
+{
+    public class GetUserResolver : IValueResolver<TaskCreateViewModel, Models.Task, User>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserResolver(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public User Resolve(TaskCreateViewModel source, Models.Task destination, User destMember, ResolutionContext context)
+        {
+            return _userRepository.GetById(source.CreatorId);
+        }
+    }
+}
