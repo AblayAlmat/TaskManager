b7d1fa2f1e720d0d1f885cd1305a1830d8cc6ad2
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-03 00:23:06 +0600

    Добавил модель представления для авторизации

---
+++ TaskManager/ViewModels/Account/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskManager.ViewModels.Account
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Введите имя пользователя")]
+        public string Login { get; set; }
+        [Required(ErrorMessage = "Введите пароль")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
