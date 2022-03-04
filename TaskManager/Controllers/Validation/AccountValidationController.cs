27a2838e7e592147b1cd5ec99b5d2f195a0adc51
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-05 00:04:33 +0600

    Добавил валидацию на существующие имена пользователей или email-ы

---
+++ TaskManager/Controllers/Validation/AccountValidationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using TaskManager.Models.DbContext;
+
+namespace TaskManager.Controllers.Validation
+{
+    public class AccountValidationController : Controller
+    {
+        private readonly TaskManagerContext _context;
+
+        public AccountValidationController(TaskManagerContext context)
+        {
+            _context = context;
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public bool ValidateEmail(string email)
+        {
+            email = email.Trim().ToLower();
+            return !_context.Users.Any(u => u.Email == email);
+        }
+
+        [AcceptVerbs("GET", "POST")]
+        public bool ValidateUserName(string userName)
+        {
+            userName = userName.Trim().ToLower();
+            return !_context.Users.Any(u => u.UserName == userName);
+        }
+    }
+}
--- TaskManager/ViewModels/Account/RegisterViewModel.cs
+++ TaskManager/ViewModels/Account/RegisterViewModel.cs
@@ -1,12 +1,15 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace TaskManager.ViewModels.Account
 {
     public class RegisterViewModel
     {
         [Required(ErrorMessage = "Введите имя пользователя")]
+        [Remote("ValidateUserName", "AccountValidation", ErrorMessage = "Введенное имя пользователя занято")]
         public string UserName { get; set; }
         [Required(ErrorMessage = "Введите адрес эл.почты")]
+        [Remote("ValidateEmail", "AccountValidation", ErrorMessage = "Введенный адрес почты занят")]
         public string Email { get; set; }
         [Required(ErrorMessage = "Введите пароль")]
         [DataType(DataType.Password)]
