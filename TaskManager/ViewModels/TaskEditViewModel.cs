22b9264dde3e3615e1315c96be125eb34906dd2e
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-12 13:15:31 +0600

    Добавил модель представления для внесения изменений по задаче

---
+++ TaskManager/ViewModels/TaskEditViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using TaskManager.Enums;
+
+namespace TaskManager.ViewModels
+{
+    public class TaskEditViewModel
+    {
+        public string Id { get; set; }
+        [Required(ErrorMessage = "Введите название задачи")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Введите описание задачи")]
+        public string Description { get; set; }
+        [Required(ErrorMessage = "Выберите приоритет задачи")]
+        public Priority Priority { get; set; }
+        [Required(ErrorMessage = "Введите дату дедлайна задачи")]
+        public DateTime Deadline { get; set; }
+    }
+}
