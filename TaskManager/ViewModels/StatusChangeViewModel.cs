c1401b54a1394df1281599c737b9dee795a4b8a9
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-12 11:53:09 +0600

    Добавил модель представления для смены статуса задачи

---
+++ TaskManager/ViewModels/StatusChangeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using TaskManager.Enums;
+
+namespace TaskManager.ViewModels
+{
+    public class StatusChangeViewModel
+    {
+        public string TaskId { get; set; }
+        public Status CurrentStatus { get; set; }
+        public string ExecutorId { get; set; }
+    }
+}
