70657f72645454b10f3322facf72683d53dce6e4
Umirkhanov Kuanysh <kuanysh79a@gamil.com>
2022-03-12 13:16:21 +0600

    Добавил файл конфигурации по конвертации из модели задачи в модель представления по внесению изменений в задачу

---
+++ TaskManager/MappingConfigurations/Task/TaskEditViewModelProfile.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using TaskManager.ViewModels;
+
+namespace TaskManager.MappingConfigurations.Task
+{
+    public class TaskEditViewModelProfile : Profile
+    {
+        public TaskEditViewModelProfile()
+        {
+            CreateMap<Models.Task, TaskEditViewModel>()
+                .ForMember(dest => dest.Id, opt =>
+                    opt.MapFrom(t => t.Id))
+                .ForMember(dest => dest.Name, opt =>
+                    opt.MapFrom(t => t.Name))
+                .ForMember(dest => dest.Description, opt =>
+                    opt.MapFrom(t => t.Description))
+                .ForMember(dest => dest.Deadline, opt =>
+                    opt.MapFrom(t => t.Deadline))
+                .ForMember(dest => dest.Priority, opt =>
+                    opt.MapFrom(t => t.Priority));
+        }
+    }
+}
